3ab432024f705069909721c91d5bf0a78bff5779
popovaver7-wq <popovaver7@gmail.com>
2026-03-14 13:56:04 +0100

    0.4

--- Assets/DragPiece.cs
+++ Assets/DragPiece.cs
@@ -7,9 +7,12 @@ public class DragPiece : MonoBehaviour
 
     private void OnMouseDown()
     {
+       
         Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        offset = transform.position - new Vector3(mouseWorld.x, mouseWorld.y, 0);
+        offset = transform.position - new Vector3(mouseWorld.x, mouseWorld.y, 0) ;
         isDragging = true;
+        Debug.Log("kliknieto element puzzla");
+        Debug.Log("isDragging po kliknieciu:" + isDragging);
     }
 
     private void OnMouseUp()
