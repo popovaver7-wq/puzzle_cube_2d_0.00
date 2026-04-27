421bf68610a63ac55928b4f45dd7966bb6334e91
popovaver7-wq <popovaver7@gmail.com>
2026-04-27 05:31:39 +0200

    0.6

--- Assets/QuizManager1.cs
+++ Assets/QuizManager1.cs
@@ -1,7 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
-
-using UnityEngine;
 using TMPro;
 
 public class QuizManager : MonoBehaviour
@@ -25,11 +22,97 @@ public class QuizManager : MonoBehaviour
         questionPanel.SetActive(false);
     }
 
-    public void CheckAnswer()
+    // Вопрос 1
+    public void CheckAnswer1()
     {
         string answer = answerInput.text.ToLower();
 
         if (answer == "jelen")
+        {
+            answeredCorrectly = true;
+            questionPanel.SetActive(false);
+            resultImage.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("zla odpowiedz");
+        }
+    }
+
+    // Вопрос 2
+    public void CheckAnswer2()
+    {
+        string answer = answerInput.text.ToLower();
+
+        if (answer == "kot")
+        {
+            answeredCorrectly = true;
+            questionPanel.SetActive(false);
+            resultImage.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("zla odpowiedz");
+        }
+    }
+
+    // Вопрос 3
+    public void CheckAnswer3()
+    {
+        string answer = answerInput.text.ToLower();
+
+        if (answer == "motyl")
+        {
+            answeredCorrectly = true;
+            questionPanel.SetActive(false);
+            resultImage.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("zla odpowiedz");
+        }
+    }
+
+    // Вопрос 4
+    public void CheckAnswer4()
+    {
+        string answer = answerInput.text.ToLower();
+
+        if (answer == "wielblad")
+        {
+            answeredCorrectly = true;
+            questionPanel.SetActive(false);
+            resultImage.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("zla odpowiedz");
+        }
+    }
+
+    // Вопрос 5
+    public void CheckAnswer5()
+    {
+        string answer = answerInput.text.ToLower();
+
+        if (answer == "kangur")
+        {
+            answeredCorrectly = true;
+            questionPanel.SetActive(false);
+            resultImage.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("zla odpowiedz");
+        }
+    }
+
+    // Вопрос 6
+    public void CheckAnswer6()
+    {
+        string answer = answerInput.text.ToLower();
+
+        if (answer == "kogut")
         {
             answeredCorrectly = true;
             questionPanel.SetActive(false);
